bdee1f3def87cc97c2ed351baf3da26ee619fa78
Daniel <daniel.moderiano@outlook.com>
2021-09-22 22:48:35 +0930

    Add basic HTML app layout, commenting where React components will be needed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './styles/reset.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <header className="header">
+        <h1 className="header__title">Memory Game</h1>
+        <div className="scoreboard">
+          {/* Component to display current score */}
+          {/* Component to display best score - may be combined with above? */}
+        </div>
+      </header>
+      <main className="main">
+        <div className="instructions">Increase your score by clicking on an image, but if you click the same image twice, it's game over!</div>
+      </main>
+    </div>
   );
 }
 
