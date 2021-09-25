b08bd1a524557e4fa0efb1da770363fcffd3d2d5
Daniel <daniel.moderiano@outlook.com>
2021-09-25 15:45:40 +0930

    Add shuffle on click and load, and add images to render

--- src/App.js
+++ src/App.js
@@ -1,41 +1,97 @@
 import './styles/reset.css';
 import './styles/App.css';
 
+import mario from './images/mario.png';
+import bowser from './images/bowser.png';
+import diddy from './images/diddy.png';
+import fox from './images/fox.png';
+import ike from './images/ike.png';
+import isabelle from './images/isabelle.png';
+import link from './images/link.png';
+import luigi from './images/luigi.png';
+import peach from './images/peach.png';
+import wario from './images/wario.png';
+import joker from './images/joker.png';
+import dk from './images/dk.png';
+
+
 import { Card } from './components/Card';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 
 function App() {
 
   const [characters, setCharacters] = useState({
-    cloud: {
-      id: nanoid(),
-      clicked: false,
-    },
     mario: {
       id: nanoid(),
       clicked: false,
-    },
-    kirby: {
-      id: nanoid(),
-      clicked: false,
+      src: mario,
     },
     'donkey kong': {
       id: nanoid(),
       clicked: false,
+      src: dk,
     },
-    richter: {
+    bowser: {
       id: nanoid(),
       clicked: false,
+      src: bowser,
+    },
+    'diddy kong': {
+      id: nanoid(),
+      clicked: false,
+      src: diddy,
+    },
+    fox: {
+      id: nanoid(),
+      clicked: false,
+      src: fox,
+    },
+    ike: {
+      id: nanoid(),
+      clicked: false,
+      src: ike,
+    },
+    isabelle: {
+      id: nanoid(),
+      clicked: false,
+      src: isabelle,
+    },
+    luigi: {
+      id: nanoid(),
+      clicked: false,
+      src: luigi,
+    },
+    link: {
+      id: nanoid(),
+      clicked: false,
+      src: link,
+    },
+    peach: {
+      id: nanoid(),
+      clicked: false,
+      src: peach,
+    },
+    wario: {
+      id: nanoid(),
+      clicked: false,
+      src: wario,
+    },
+    joker: {
+      id: nanoid(),
+      clicked: false,
+      src: joker,
     }
   });
 
-  Object.keys(characters).forEach((character) => {
-    console.log(`${character} has id ${characters[character].id}`);
-  })
-
   const [currentScore, setCurrentScore] = useState(0);
+
+  useEffect(() => {
+    if (currentScore === (Object.keys(characters)).length) {
+      alert('Max score reached, well done! Play again?');
+    }
+  });
 
   const markCharacterAsClicked = (characterName) => {
     setCharacters({
@@ -61,6 +117,7 @@ function App() {
       markCharacterAsClicked(characterName);
       incrementCurrentScore();
     }
+
   }
 
   // From Mike Bostock https://bost.ocks.org/mike/shuffle - implementation of Fisher Yates Shuffle in JS
@@ -80,18 +137,12 @@ function App() {
     return array;
   }
 
-  const charactersToRender = () => {
-    return Object.keys(characters).map((character) => (<Card key={characters[character].id} name={character} handleClick={handleCardClick}/>));
-  }
-
-
+  // Randomly shuffle the characters in the state. Because this function is returned by App.js, it will be re-rendered/run on each click, enabling shuffling of cards on each click
   const shuffleCharacterCards = () => {
     const shuffledCharacters = shuffleArray(Object.keys(characters));
-    console.log(shuffledCharacters);
     
-    return shuffledCharacters.map((character) => (<Card key={characters[character].id} name={character} handleClick={handleCardClick}/>));
+    return shuffledCharacters.map((character) => (<Card key={characters[character].id} name={character} handleClick={handleCardClick} src={characters[character].src} />));
   }
-
 
   return (
     <div className="App">
@@ -106,7 +157,6 @@ function App() {
         <div className="instructions">Increase your score by clicking on an image, but if you click the same image twice, it's game over!</div>
         <div className="container">
           {/* Use container display grid to layout cards */}
-          {charactersToRender()}
           {shuffleCharacterCards()}
           {/* Each card should be a reused component 'card' with an image, name, and background */}
         </div>
@@ -114,5 +164,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export const Card = (props) => {
   return (
     <div className="card" onClick={() => {props.handleClick(props.name)}}>
       <div className="card__container">
-        <img src="" alt="" className="card__img" />
+        <img src={props.src} alt={props.name} className="card__img" />
       </div>
       <h2 className="card__title">{props.name.toUpperCase()}</h2>
     </div>
